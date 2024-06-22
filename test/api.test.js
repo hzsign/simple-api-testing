d92a9444c3769704c24651e46592bf0e65605c6a
irfannurkholik748@gmail.com <nauliadewi19@gmail.com>
2024-06-22 22:45:52 +0700

    Update to use ES modules

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,17 @@
-const axios = require("axios");
-const { expect } = require("chai");
+import axios from "axios";
+import { expect } from "chai";
 
 describe("API Testing", () => {
   it("should return status 200 for GET /posts/1", async () => {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    expect(response.status).to.equal(200);
+  });
+
+  it("should return status 200 for GET /comments/1", async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments/1"
     );
     expect(response.status).to.equal(200);
   });
